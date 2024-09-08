65f128ff0c601e2c636d12b9debf0a18cd555c2b
nahco314 <nahco3_ta@yahoo.co.jp>
2024-09-09 07:25:19 +0900

    feat: add onefmt_plugin_setup

--- src/interface.rs
+++ src/interface.rs
@@ -1,28 +1,53 @@
-extern crate alloc;
+#[macro_export]
+macro_rules! onefmt_plugin_setup {
+    ( $f:ident ) => {
+        pub fn to_array_result(arr: &[u8]) -> *mut u8 {
+            let mut new_vec = (arr.len() as u64).to_le_bytes().to_vec();
 
-use core::alloc::Layout;
+            new_vec.append(&mut arr.to_vec());
 
-pub fn to_array_result(arr: &[u8]) -> *mut u8 {
-    let mut new_vec = (arr.len() as u64).to_le_bytes().to_vec();
+            let ptr = new_vec.as_mut_ptr();
+            std::mem::forget(new_vec);
 
-    new_vec.append(&mut arr.to_vec());
+            ptr
+        }
 
-    let ptr = new_vec.as_mut_ptr();
-    std::mem::forget(new_vec);
+        #[no_mangle]
+        pub unsafe extern "C" fn of_malloc(size: u32, alignment: u32) -> *mut u8 {
+            extern crate alloc;
 
-    ptr
-}
+            use core::alloc::Layout;
 
-#[no_mangle]
-pub unsafe extern "C" fn of_malloc(size: u32, alignment: u32) -> *mut u8 {
-    let layout = Layout::from_size_align_unchecked(size as usize, alignment as usize);
-    alloc::alloc::alloc(layout)
-}
+            let layout = Layout::from_size_align_unchecked(size as usize, alignment as usize);
+            alloc::alloc::alloc(layout)
+        }
 
-#[no_mangle]
-pub unsafe extern "C" fn of_free(ptr: *mut u8, size: u32, alignment: u32) {
-    let layout = Layout::from_size_align_unchecked(size as usize, alignment as usize);
-    alloc::alloc::dealloc(ptr, layout);
+        #[no_mangle]
+        pub unsafe extern "C" fn of_free(ptr: *mut u8, size: u32, alignment: u32) {
+            extern crate alloc;
+
+            use core::alloc::Layout;
+
+            let layout = Layout::from_size_align_unchecked(size as usize, alignment as usize);
+            alloc::alloc::dealloc(ptr, layout);
+        }
+
+        #[no_mangle]
+        pub extern "C" fn main(ptr: *mut u8, len: usize) -> i32 {
+            use serde_json;
+
+            let slice = unsafe { std::slice::from_raw_parts(ptr, len) };
+            let v = serde_json::from_slice(slice).unwrap();
+
+            let result = $f(v);
+
+            let b = serde_json::to_vec(&result).unwrap();
+            let result = b.as_slice();
+
+            let result = to_array_result(result);
+            result as i32
+        }
+    };
 }
 
 #[macro_export]
